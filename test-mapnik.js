e4c3e64dc8550a2f2d1cf22cf8493eb102539de6
James Badger <james@jamesbadger.ca>
2018-05-24 16:14:33 -0600

    Add mapnik test script
    Will do a quick render to check if the stylesheet is working.

---
+++ test-mapnik.js
@@ -0,0 +1,24 @@
+// Use mapnik to draw a sample map
+const fs     = require('fs');
+const mapnik = require('mapnik');
+
+// register fonts and datasource plugins
+mapnik.register_default_fonts();
+mapnik.register_default_input_plugins();
+
+var map = new mapnik.Map(256, 256);
+map.load('./styles/nedata.xml', (err,map) => {
+    if (err) throw err;
+    map.zoomAll();
+    var im = new mapnik.Image(256, 256);
+    map.render(im, (err,im) => {
+      if (err) throw err;
+      im.encode('png', (err,buffer) => {
+          if (err) throw err;
+          fs.writeFile('test-mapnik.png',buffer, (err) => {
+              if (err) throw err;
+              console.log('saved map image to test-mapnik.png');
+          });
+      });
+    });
+});
