6f5dd1a5468e0ef6a4f9256577a6efa6e5f18125
James Badger <james@jamesbadger.ca>
2018-05-24 00:14:28 -0600

    Use conditional GET requests
    Will avoid re-downloading file if it has not changed. Requires
    remote server to support last-modified header.

--- get-data.js
+++ get-data.js
@@ -1,38 +1,46 @@
 // Download the shapefiles for the stylesheet
-const path  = require('path');
-const fs    = require('fs');
-const http  = require('http');
-const https = require('https');
-const URL   = require('url');
+const path   = require('path');
+const fs     = require('fs');
+const http   = require('http');
+const https  = require('https');
+const moment = require('moment');
+const URL    = require('url');
 
 const data = require('./data.json');
 
-function download(uri, filepath) {
-  const url = URL.parse(uri);
+function schemeForURI(uri) {
+  return uri.startsWith('https') ? https : http;
+}
 
-  if (uri.startsWith('https')) {
-    scheme = https;
-  } else {
-    scheme = http;
+function download(uri, filepath) {
+  let url          = URL.parse(uri);
+  let scheme       = schemeForURI(uri);
+  let options = {
+    method:   'GET',
+    hostname: url.hostname,
+    path:     url.pathname
+  };
+
+  if (fs.existsSync(filepath)) {
+    console.log("File already exists, checking modification time");
+    let stats = fs.statSync(filepath);
+    options.headers = options.headers || {};
+    options.headers['if-modified-since'] = moment(stats.mtime).utc().format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
   }
 
-  let file = fs.createWriteStream(filepath);
-  let fileprogress = 0;
-  let filetotal = 1;
-  let request = scheme.request({
-    method: 'GET',
-    hostname: url.hostname,
-    path: url.pathname
-  }, (response) => {
-    console.log(response.statusCode);
-    console.log(response.headers);
-
+  let request = scheme.request(options, (response) => {
     // Handle redirects
-    if (response.statusCode >= 300 && response.statusCode < 400 && response.headers['location'] !== undefined) {
+    if (response.statusCode >= 301 && response.statusCode < 304 && response.headers['location'] !== undefined) {
       console.log("Following redirect");
-      fs.unlinkSync(filepath);
       download(response.headers['location'], filepath);
+    } else if (response.statusCode == 304) {
+      console.log("No update required.");
     } else {
+      console.log(`Downloading to "${filepath}"`);
+      let fileprogress = 0;
+      let filetotal    = 1;
+      let file         = fs.createWriteStream(filepath);
+
       // Print file download progress
       filetotal = parseInt(response.headers['content-length']);
       response.on('data', (chunk) => {
@@ -66,11 +74,5 @@ data.forEach((datum) => {
   let filename = path.basename(datum.src);
   let filepath = "data/" + filename;
 
-  // If file exists, do a conditional GET.
-  if (fs.existsSync(filepath) && fs.statSync(filepath).size !== 0) {
-    console.log(`"${datum.name}" exists at "${filepath}", checking for update`);
-  } else {
-    console.log(`Downloading "${datum.name}" to "${filepath}"`);
-    download(datum.src, filepath);
-  }
+  download(datum.src, filepath);
 });
