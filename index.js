d32a3b1ab73f27a483c8b7920e95f00bfc7dc5a3
James Badger <james@jamesbadger.ca>
2018-05-25 13:29:30 -0600

    Extract Mapnik Render to class

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const express = require('express');
-const fs      = require('fs');
-const mapnik  = require('mapnik');
-const proj4   = require('proj4');
-const wms     = require('./src/wms111');
-
-// register fonts and datasource plugins
-mapnik.register_default_fonts();
-mapnik.register_default_input_plugins();
+const express      = require('express');
+const fs           = require('fs');
+const MapnikRender = require('./src/mapnik_render');
+const wms          = require('./src/wms111');
 
 let app = express();
 
@@ -14,37 +9,6 @@ let fakeTile = fs.readFileSync('grad.png');
 let fakeHeaders = {
   'content-type': 'image/png'
 };
-
-// Read a Mapnik XML stylesheet from a path, and use options to generate an
-// image. Options:
-// * bbox   - array of four floats delineating region to render
-// * format - MIME type of output
-// * height - pixel height of output image
-// * srs    - PROJ4 style projection definition of output map (geographic, not screen)
-// * width  - pixel width of output image
-//
-// Returns a Promise that resolves with a buffer containing the image, or
-// rejects with an error.
-function mapnikRender(stylesheetPath, options) {
-  let map = new mapnik.Map(options.width, options.height);
-
-  return new Promise((resolve, reject) => {
-    map.load(stylesheetPath, (err, map) => {
-      if (err) reject(err);
-
-      map.zoomToBox(options.bbox);
-      let image = new mapnik.Image(options.width, options.height);
-
-      map.render(image, (err, image) => {
-        if (err) reject(err);
-        image.encode('png', (err, buffer) => {
-            if (err) reject(err);
-            resolve(buffer);
-        });
-      });
-    });
-  });
-}
 
 let service = {
   title: 'D162 WMS',
@@ -62,7 +26,8 @@ let service = {
       image: ['png'],
       // Return a buffer
       getMap: (params) => {
-        return mapnikRender('styles/nedata.xml', {
+        let style = new MapnikRender('styles/nedata.xml');
+        return style.renderImage({
           width: parseInt(params.WIDTH),
           height: parseInt(params.HEIGHT),
           bbox: params.BBOX.split(',').map(parseFloat),
---
+++ src/mapnik_render.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const mapnik = require('mapnik');
+
+mapnik.register_default_fonts();
+mapnik.register_default_input_plugins();
+
+class MapnikRender {
+  constructor(stylesheetPath) {
+    this.stylesheetPath = stylesheetPath;
+    this.map = new mapnik.Map(256, 256);
+  }
+
+  // Generate an image using the stylesheet. Options:
+  // * bbox   - array of four floats delineating region to render
+  // * format - MIME type of output
+  // * height - pixel height of output image
+  // * srs    - PROJ4 style projection definition of output map (geographic, not screen)
+  // * width  - pixel width of output image
+  //
+  // Returns a Promise that resolves with a buffer containing the image, or
+  // rejects with an error.
+  renderImage(options) {
+    this.map.resize(options.width, options.height);
+
+    return new Promise((resolve, reject) => {
+      this.map.load(this.stylesheetPath, (err, map) => {
+        if (err) reject(err);
+
+        map.zoomToBox(options.bbox);
+        let image = new mapnik.Image(options.width, options.height);
+
+        map.render(image, (err, image) => {
+          if (err) reject(err);
+          image.encode('png', (err, buffer) => {
+              if (err) reject(err);
+              resolve(buffer);
+          });
+        });
+      });
+    });
+  }
+}
+
+module.exports = MapnikRender;
