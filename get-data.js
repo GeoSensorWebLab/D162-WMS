7df86094c510b765d880149639abf9653392610b
James Badger <james@jamesbadger.ca>
2018-08-10 18:08:40 -0600

    Use JS Promise to prevent extract before download
    Also fixes missing break

--- get-data.js
+++ get-data.js
@@ -16,61 +16,67 @@ function schemeForURI(uri) {
 }
 
 function download(uri, filepath) {
-  let url          = URL.parse(uri);
-  let scheme       = schemeForURI(uri);
-  let options = {
-    method:   'GET',
-    hostname: url.hostname,
-    path:     url.pathname
-  };
+  let promise = new Promise((resolve, reject) => {
+    let url     = URL.parse(uri);
+    let scheme  = schemeForURI(uri);
+    let options = {
+      method:   'GET',
+      hostname: url.hostname,
+      path:     url.pathname
+    };
 
-  if (fs.existsSync(filepath)) {
-    console.log("File already exists, checking modification time");
-    let stats = fs.statSync(filepath);
-    options.headers = options.headers || {};
-    options.headers['if-modified-since'] = moment(stats.mtime).utc().format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
-  }
-
-  let request = scheme.request(options, (response) => {
-    // Handle redirects
-    if (response.statusCode >= 301 && response.statusCode < 304 && response.headers['location'] !== undefined) {
-      console.log("Following redirect");
-      download(response.headers['location'], filepath);
-    } else if (response.statusCode == 304) {
-      console.log("No update required.");
-    } else {
-      console.log(`Downloading to "${filepath}"`);
-      let fileprogress = 0;
-      let filetotal    = 1;
-      let file         = fs.createWriteStream(filepath);
-
-      // Print file download progress
-      filetotal = parseInt(response.headers['content-length']);
-      response.on('data', (chunk) => {
-        fileprogress += chunk.length;
-        let progress = 100 * fileprogress / filetotal;
-        process.stdout.write(`${fileprogress}B/${filetotal}B ${progress.toFixed(1)}%\r`);
-      });
-
-      // File download is finished; update the file modification date to match
-      // the "last-modified" HTTP header for caching purposes.
-      response.on('end', () => {
-        file.end();
-        console.log("\nDownload Complete.");
-        let moddate = new Date(response.headers['last-modified']);
-        fs.utimesSync(filepath, moddate, moddate);
-      });
-
-      response.pipe(file);
+    if (fs.existsSync(filepath)) {
+      console.log("File already exists, checking modification time");
+      let stats = fs.statSync(filepath);
+      options.headers = options.headers || {};
+      options.headers['if-modified-since'] = moment(stats.mtime).utc().format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
     }
+
+    let request = scheme.request(options, (response) => {
+      // Handle redirects
+      if (response.statusCode >= 301 && response.statusCode < 304 && response.headers['location'] !== undefined) {
+        console.log("Following redirect");
+        resolve(download(response.headers['location'], filepath));
+      } else if (response.statusCode == 304) {
+        console.log("No update required.");
+        resolve();
+      } else {
+        console.log(`Downloading to "${filepath}"`);
+        let fileprogress = 0;
+        let filetotal    = 1;
+        let file         = fs.createWriteStream(filepath);
+
+        // Print file download progress
+        filetotal = parseInt(response.headers['content-length']);
+        response.on('data', (chunk) => {
+          fileprogress += chunk.length;
+          let progress = 100 * fileprogress / filetotal;
+          process.stdout.write(`${fileprogress}B/${filetotal}B ${progress.toFixed(1)}%\r`);
+        });
+
+        // File download is finished; update the file modification date to match
+        // the "last-modified" HTTP header for caching purposes.
+        response.on('end', () => {
+          file.end();
+          console.log("\nDownload Complete.");
+          let moddate = new Date(response.headers['last-modified']);
+          fs.utimesSync(filepath, moddate, moddate);
+          resolve();
+        });
+
+        response.pipe(file);
+      }
+    });
+
+    request.on('error', (err) => {
+      fs.unlinkSync(filepath);
+      console.error("Failed to download file", err);
+      reject();
+    });
+    request.end();
   });
 
-  request.on('error', (err) => {
-    fs.unlinkSync(filepath);
-    console.error("Failed to download file", err);
-    process.exit(1);
-  });
-  request.end();
+  return promise;
 }
 
 // Unpack the archive.
@@ -81,6 +87,7 @@ function extract(archive) {
   switch(path.extname(archive)) {
     case '.zip':
     extractZIP(archive);
+    break;
     default:
     console.warn(`No extraction handler implemented for ${archive}`);
   }
@@ -138,6 +145,7 @@ data.forEach((datum) => {
   let filename = path.basename(datum.src);
   let filepath = "data/" + filename;
 
-  download(datum.src, filepath);
-  extract(filepath);
+  download(datum.src, filepath).then(function() {
+    extract(filepath);
+  });
 });
