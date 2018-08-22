105a3e765d91f508996d6ab777bb70bf3f2a581d
James Badger <james@jamesbadger.ca>
2018-08-22 14:50:20 +0000

    Register system fonts for Mapnik
    Otherwise it won't find the fonts for the stylesheets

--- src/mapnik_style.js
+++ src/mapnik_style.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const mapnik = require('mapnik');
 
 mapnik.register_default_fonts();
+mapnik.register_system_fonts();
 mapnik.register_default_input_plugins();
 
 class MapnikStyle {
