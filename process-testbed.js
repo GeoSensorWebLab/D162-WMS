30864352c261c2ec1f66410e826dbf6c4de67c69
James Badger <james@jamesbadger.ca>
2018-08-28 16:27:44 -0600

    Remove unused script

--- process-testbed.js
+++
@@ -1,9 +0,0 @@
-// Check for the necessary source data for Testbed 14
-// If it exists in the data directory, decompress it and process it so the
-// OSM attributes are available for CartoCSS stylesheets.
-
-// Cases:
-// * extracted dataset doesn't exist, archive doesn't exist
-// * extracted dataset doesn't exist, archive exists
-// * extracted dataset exists, it has not been processed
-// * extracted dataset exists, it has been processed
