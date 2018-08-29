17b9c42b43edd0ca91d377f992ec4d8e9af3e958
James Badger <james@jamesbadger.ca>
2018-08-28 18:17:43 -0600

    Add second WMS layer for Daraa

--- index.js
+++ index.js
@@ -39,6 +39,33 @@ let service = {
           srs:    params.SRS
         });
       }
+    },
+    {
+      // human-readable title
+      title: 'Testbed 14 Daraa',
+      // machine-readable name
+      name: 'testbed',
+      // EPSG:4326 bounding box for layer
+      bbox: [-180, -85, 180, 85],
+      // An extent object is required for each projection supported.
+      // The bbox should be in the coordinates of the SRS.
+      bounds: [{
+        srs:  'EPSG:3857',
+        bbox: [-20026376.39, -20048966.10, 20026376.39, 20048966.10]
+      }],
+      // Rendering output formats supported
+      formats: ['image/png'],
+      // Return a Promise that resolves with a Buffer containing the image
+      getMap: (params) => {
+        let style = new MapnikStyle('styles/testbed.xml');
+        return style.renderImage({
+          format: params.FORMAT,
+          width:  parseInt(params.WIDTH),
+          height: parseInt(params.HEIGHT),
+          bbox:   params.BBOX.split(',').map(parseFloat),
+          srs:    params.SRS
+        });
+      }
     }
   ]
 };
